9ceb5b3c08729f70b8ab0e14812d97ff3fbfd0aa
alissonfonseca9 <94880360+alissonfonseca9@users.noreply.github.com>
2026-05-20 23:47:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'alissonfonsecasuporte.netlify.app'
+    origin: 'https://alissonfonsecasuporte.netlify.app/'
 }));
 
 app.use(express.json());
